21fed58076fb7991703e65f366f0ad42d5ba8655
wachiravit01 <wachiravit24994@gmail.com>
2025-11-13 04:32:15 +0000

    Added the test case

--- calculator.py
+++ calculator.py
@@ -3,23 +3,23 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+        for i in range(b):
             result = self.add(result, a)
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
--- test_calculator.py
+++ test_calculator.py
@@ -11,5 +11,37 @@ class TestCalculator(unittest.TestCase):
 
     # Add the following test methods to the TestCalculator class:
 
+    def test_add_positive(self):
+        self.assertEqual(self.calc.add(2, 2), 4)
+
+    def test_add_negative(self):
+        self.assertEqual(self.calc.add(-5, -3), -8)
+
+    def test_subtract_positive(self):
+        self.assertEqual(self.calc.subtract(10, 4), 6)
+
+    def test_subtract_negative_result(self):
+        self.assertEqual(self.calc.subtract(3, 7), -4)
+
+    def test_multiply_positive(self):
+        self.assertEqual(self.calc.multiply(3, 5), 15)
+
+    def test_multiply_by_zero(self):
+        self.assertEqual(self.calc.multiply(7, 0), 0)
+
+    def test_divide_normal(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+
+    def test_divide_with_remainder(self):
+        self.assertAlmostEqual(self.calc.divide(7, 2), 3)
+
+    def test_modulo_normal(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+
+    def test_modulo_exect(self):
+        self.assertEqual(self.calc.modulo(6, 3),0)
+
+    
+
 if __name__ == '__main__':
     unittest.main()
